d27ad9714c48d10a7c87b61cf305538d33e34d9e
doresolla <dondupova02@mail.ru>
2023-04-22 17:55:30 +0800

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -6,6 +6,8 @@ import uuid
 h_name = socket.gethostname()
 conn = http.client.HTTPConnection("ifconfig.me")
 conn.request("GET", "/ip")
+ip_address=socket.gethostbyname(h_name)
 print("IP address: " + str(conn.getresponse().read()))
+print("IP address local: " + ip_address)
 print("Host Name:" + h_name)
 print(':'.join(re.findall('..', '%012x' % uuid.getnode())))
